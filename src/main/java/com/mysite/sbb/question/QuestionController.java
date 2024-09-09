c4c9d95105ca097ab99065a45b0ee3c24d4bc8b1
imhansung <imhansung91@gmail.com>
2024-09-09 11:35:44 +0900

    2-10 상세 페이지 만들기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,4 +21,13 @@ public class QuestionController {
 		model.addAttribute("questionList", questionList);
 		return "question_list";
 	}
+	
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
+		
+	}
+	
 }
